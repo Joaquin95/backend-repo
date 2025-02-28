ae2cd79aa850769295ed59705ad5ac1f1f6fa229
Joaquin <Mintinvestments95@gmail.com>
2025-02-28 10:08:58 -0600

    updated server.js

--- server.js
+++ server.js
@@ -10,13 +10,18 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://frontend-repo-1ebj.onrender.com"],
+    origin: ["https://frontend-repo-1ebj.onrender.com",
+    ],
     credentials: true,
   })
 );
 
 // Routes
 app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Server is running.");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
