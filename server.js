c8c3f11afc2d104eedfc7748eeb02eb729e2dd7f
Joaquin <Mintinvestments95@gmail.com>
2025-02-28 09:26:21 -0600

    updated server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend.onrender.com"],
+    origin: ["http://localhost:5173", "https://frontend-repo-1ebj.onrender.com"],
     credentials: true,
   })
 );
