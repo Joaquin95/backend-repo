cbe0cae03aa848d746a376272515231fdc8f6e52
Joaquin <Mintinvestments95@gmail.com>
2025-03-04 11:51:51 -0600

    updated db

--- db.js
+++ db.js
@@ -4,10 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pkg;
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, 
-  ssl: { rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 export default pool;
