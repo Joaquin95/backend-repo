fd8411249bbc44af502222178adf7c416701543c
Joaquin <Mintinvestments95@gmail.com>
2025-04-24 17:33:01 -0500

    updated backends db.js for new render postgreSQL

--- db.js
+++ db.js
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pkg;
-const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
